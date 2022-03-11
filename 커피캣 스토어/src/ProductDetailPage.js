cc77736c84081d8dd4d0b9c9e8b12a36331ebec3
Yeewon <yes3427@kookmin.ac.kr>
2022-03-12 00:07:01 +0900

    refactor: 불필요한 코드 제거

--- 커피캣 스토어/src/ProductDetailPage.js
+++ 커피캣 스토어/src/ProductDetailPage.js
@@ -1,17 +1,6 @@
 import ProductDetail from "./ProductDetail.js";
 import { request } from "./utils/api.js";
 
-/*
-    {
-      "id": 24,
-      "name": "10개 묶음",
-      "price": 0,
-      "stock": 555,
-      "created_at": "2021-08-23T23:03:04.873Z",
-      "updated_at": "2021-08-23T23:03:04.879Z"
-    }
-
-*/
 export default function ProductDetailPage({ $target, productId }) {
   this.state = {
     productId,
